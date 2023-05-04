a19ef0f0f74017f3326cc23a47099c3717181205
Rodrigo Bitu <rodrigo.moraes@enacom.com.br>
2023-05-03 23:55:02 -0300

    feat: create list of books

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,13 +2,17 @@ import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
 
 import { ConversationModule } from './conversation/conversation.module';
-import { PrismaService } from './prisma.service';
+import { PrismaService } from './database/prisma.service';
+import { BooksModule } from './books/books.module';
+import { UsersModule } from './users/users.module';
 import configuration from './config/default';
 
 @Module({
   imports: [
     ConfigModule.forRoot({ isGlobal: true, load: [configuration] }),
     ConversationModule,
+    BooksModule,
+    UsersModule,
   ],
   controllers: [],
   providers: [PrismaService],
